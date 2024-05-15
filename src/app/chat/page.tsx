4ed6f0e3991fb3bcf835e6a6a32f5e324ac58558
oboard <oboard@outlook.com>
2024-05-15 23:30:51 +0800

    fix: lint

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -18,7 +18,7 @@ const CodeBlock = ({
   children,
 }: {
   language: string | undefined;
-  children: React.ReactNode;
+  children: string;
 }) => {
   return (
     <SyntaxHighlighter
@@ -55,6 +55,7 @@ const genUuid = () => {
   // bits 12-15 of the time_hi_and_version field to 0010
   s[14] = "4";
   // bits 6-7 of the clock_seq_hi_and_reserved to 01
+  // @ts-ignore
   s[19] = hexDigits.substring((s[19] & 0x3) | 0x8, 1);
   // bits 6-7 of the clock_seq_hi_and_reserved to 01
   s[8] = s[13] = s[18] = s[23] = "-";
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,6 @@ import "./globals.css";
 
 import { Toaster } from "react-hot-toast";
 
-import { motion } from "framer-motion";
-import Template from "./template";
-
 export const metadata = {
   title: "一块小板子",
   description: "一个隐蔽的网站捏QAQ",

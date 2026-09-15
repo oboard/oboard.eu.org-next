1321509a1aa53b38c365a4cb9f41460e2f5f1a20
oboard <oboard@outlook.com>
2026-09-15 23:44:31 +0800

    feat: update introduction text in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,7 +78,7 @@ export default function Home() {
                 </span>
               </h1>
               <p className="text-xl sm:text-2xl md:text-3xl text-base-content/80 animate-fade-in-up [animation-delay:800ms]">
-                计算机专业大四在读
+                已经是刚毕业的牛马了！
                 <span className="hidden sm:inline"> | </span>
                 <br className="sm:hidden" />
                 前端/移动端开发工程师
